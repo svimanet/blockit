e1a7b42811687abbb12977192c4b3d480c6a14fa
svimanet <svimanet@pm.me>
2024-09-15 22:16:53 +0200

    some new shapes, TODO rest made on sketch

--- src/utils.ts
+++ src/utils.ts
@@ -9,16 +9,48 @@ export const makeShapes = (cellsize: number): Record<Shape, FigureNode[]> => {
     { x: 0, y: cz },
     { x: cz, y: 0 },
   ];
+  const I: FigureNode[] = [
+    { x:0, y:0 },
+    { x:0, y:cz},
+    { x:0, y:cz*2},
+    { x:0, y:cz*3}
+  ];
+  const I2 = [
+    {x:0, y:0},
+    {x:cz, y:0},
+    {x:cz*2, y:0},
+    {x:cz*3, y:0},
+  ];
+  const T = [
+    {x:0, y:0},
+    {x:cz, y:0},
+    {x:cz*2, y:0},
+    {x:cz, y:cz},
+  ];
+  const S = [
+    {x:0, y:0},
+    {x:0, y:cz},
+    {x:cz, y:cz},
+    {x:cz, y:cz*2},
+  ];
+  const Z = [
+    {x:0, y:0},
+    {x:cz, y:cz},
+    {x:cz, y:0},
+    {x:cz*2, y:0},
+  ];
+
+  // T1-4, L1-4, Z1-4
 
   return {
     'O': O,
-    'I': O,
-    'I2': O,
-    'T': O,
-    'S': O,
-    'Z': O,
-    'J': O,
-    'L': O,
+    'I': I,
+    'I2': I2,
+    'T': T,
+    'S': S,
+    'Z': Z,
+    'J': Z,
+    'L': S,
   } as Record<Shape, FigureNode[]>;
 };
 
