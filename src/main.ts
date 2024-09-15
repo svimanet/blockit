0e7ade28aaab9ed7b549c75e31d90f81f57fd532
svimanet <svimanet@pm.me>
2024-09-15 13:04:01 +0200

    almost fix check lines, still buggy tho

--- src/main.ts
+++ src/main.ts
@@ -1,83 +1,10 @@
 import { Figure } from './figure';
 import { Application, DisplayObject, FederatedPointerEvent, Graphics } from 'pixi.js';
-import { grid64, makeShapes, randomShape } from './utils';
+import { makeShapes, randomShape } from './utils';
+import { checkLineCompletion, deleteEmptyFigures } from './utils/lineCompletion';
 
 console.log('Game JS loading.');
 
-/**
- * Check for any horizontal/vertical line completions,
- * and handle whatever happens
- * TODO: Not working
- * @returns 
- */
-const checkLineCompletion = () => {
-  if (figures.length < 3) return;
-
-  const rows: number[] = Array(10).fill(0);
-  const cols: number[] = Array(10).fill(0);
-
-  // For each figure, ...
-  figures.forEach((figure) => {
-    // For each rect in figure, ...
-    figure.nodes.forEach((node) => {
-      const bounds = node.getBounds();
-      // Get row and col number of rect, for counting
-      grid64.forEach((b64, i) => {
-        const xDiff = Math.abs((bounds.x - xpadding) - b64);
-        const yDiff = Math.abs((bounds.y - ypadding) - b64);
-        if (xDiff < 15) cols[i]++;
-        if (yDiff < 15) rows[i]++;
-      });
-    });
-  });
-
-  const completeRows: number[] = [];
-  rows.forEach((row: number, i: number) => {
-    if (row === 10) completeRows.push(i);
-  });
-
-  // Remove complete rows
-  if (completeRows.length > 0) {
-    console.log('complete row');
-
-    // For each complete row
-    completeRows.forEach((row) => {
-      const gridPx = grid64[row];
-
-      // For each figure
-      figures.forEach((figure) => {
-
-        /**
-         * "search" for rectangle coordinates that are within the row grid number.
-         * If found, add to nodesToDestroy array. 
-         * @param node 
-         */
-        const searchAndDestroy = (
-          node: DisplayObject,
-          inverse?:boolean
-        ): DisplayObject | undefined  => {
-          const bounds = node.getBounds();
-          let axis = bounds.y - ypadding;
-          if (inverse) axis = bounds.x - xpadding;
-
-          const diff = Math.abs(axis - gridPx);
-          if (diff < 15) return node;
-        }
-
-        // For each node in figure, ...
-        // check if the node is witin a complete row,
-        // then delete that node
-        const nodes = figure.container.children;
-        const yNodes = nodes.map(node => searchAndDestroy(node));
-        const xNodes = nodes.map(node => searchAndDestroy(node, true));
-        const nodesToDestroy = yNodes.concat(xNodes);
-        nodesToDestroy.forEach((node) => (
-          (node) && figure.container.removeChild(node)
-        ));
-      });
-    });
-  }
-}
 
 /**
  * Generate new figure to start with
@@ -155,6 +82,7 @@ let xpadding = widthDiff / 2;
 let ypadding = 200;
 
 const shapes = makeShapes(cellSize);
+const figures = Array<Figure>();
 
 // Target getter and setter for mouse event. Only one at a time on global scope.
 // Setter supplied to Figure constructor for container onclick target switching.
@@ -173,7 +101,9 @@ app.stage.on('pointerup', () => {
     const moved = dragTarget.stopMoving(figures, cellSize, sidepadding, figureStartPos);
     dragTarget = undefined;
     if (moved) {
-      checkLineCompletion();
+      console.log('figs:', figures.length);
+      checkLineCompletion(cellSize, sidepadding, figures);
+      deleteEmptyFigures(figures);
       newFigure(figureStartPos);
       incrementScore(scoreCounter, 1);
     }
@@ -186,8 +116,6 @@ renderGrid(
   cellSize,
   sidepadding
 );
-
-const figures = Array<Figure>();
 
 newFigure(figureStartPos);
 
