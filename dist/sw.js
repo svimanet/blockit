166460c2ab023ecfa6f25516466ec62ad6f9300f
svimanet <svimanet@pm.me>
2024-09-28 01:44:46 +0200

    add basic PWA

---
+++ dist/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/game.js',
+  '/manifest.json',
+  // '/images/icons/icon-192x192.png',
+  // '/images/icons/icon-512x512.png'
+];
+
+// Install event - cache static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event - serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached resource if found
+        if (response) {
+          return response;
+        }
+        // If not found, fetch the resource from the network
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event - remove old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
